cadcc990503d13ea58166a35cc74cdf7743a3316
Joe-azar <joe.azar@hotmail.com>
2025-03-09 22:00:36 +0100

    Enable v7 features in BrowserRouter to suppress future warnings

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,19 @@ import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
 import "./index.css";
 
+// ✅ Suppress future warnings by enabling v7 features early
+const routerFutureFlags = {
+  future: {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true,
+  },
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>  
-      <BrowserRouter> 
+    <AuthProvider>
+      <BrowserRouter {...routerFutureFlags}>
         <App />
       </BrowserRouter>
     </AuthProvider>
